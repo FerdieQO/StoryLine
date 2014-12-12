25c02dd361698ef6242e33910bfc34589aac8740
SJOERD VAN_DEN_BROEK <sjoerdv.d.broek@live.nl>
2014-12-12 14:27:30 +0100

    dynamic background colour

--- StoryLine/js/contextmenu.js
+++ StoryLine/js/contextmenu.js
@@ -11,13 +11,32 @@ $( window ).resize(function() {
 });
 
 $(function(){
-	$('.scenarioWrapper').on("click", function(){//, ".event"
+	$('.scenarioWrapper').on("click", function(){
 		var myIndex = $(this).index();
-		console.log(myIndex);
 		if(myIndex > 0){myIndex /= 2;}
-		$('.contextMenu').eq(myIndex).toggleClass('open');
 		$('body, .scenario-list').toggleClass('fix');
 		$('.contextMenu').eq(myIndex).animate({width:'toggle'},350);
+		$(this).toggleClass('show');//.children('.scenario')
+		if($(this).hasClass('show') && $(this).hasClass('talk'))
+		{
+			$(this).css({'background-color':'#f9c82e'});
+		}
+		else if($(this).hasClass('show') && $(this).hasClass('hold-hands'))
+		{
+			$(this).css({'background-color':'#c1392b'});
+		}
+		else if($(this).hasClass('show') && $(this).hasClass('cuddle'))
+		{
+			$(this).css({'background-color':'#5d2e4e'});
+		}
+		else if($(this).hasClass('show') && $(this).hasClass('kiss'))
+		{
+			$(this).css({'background-color':'999999'});
+		}
+		else
+		{
+			$(this).css({'background-color':'#cccccc'});
+		}
     });
 });
 
@@ -32,15 +51,27 @@ $(function(){
 });
 
 function GetPositions () {
-	$(".contextMenu").each(function(index, element) { // index = no; element = this;
+	$(".contextMenu").each(function(index, element) {
 		var myParent = $('.scenarioWrapper').eq(index);
 		var myLeft = myParent.position.left;
 		var myTop = myParent.position.top;
 		
-		if($(myParent).hasClass('talk')){$(this).css({'background-color':'#d3a30b'});}
-		if($(myParent).hasClass('hold-hands')){$(this).css({'background-color':'#8f1508'});}
-		if($(myParent).hasClass('cuddle')){$(this).css({'background-color':'#301026'});}
-		if($(myParent).hasClass('kiss')){$(this).css({'background-color':'#665454'});}
+		if($(myParent).hasClass('talk'))
+		{
+			$(this).css({'background-color':'#d3a30b'});
+		}
+		if($(myParent).hasClass('hold-hands'))
+		{
+			$(this).css({'background-color':'#8f1508'});
+		}
+		if($(myParent).hasClass('cuddle'))
+		{
+			$(this).css({'background-color':'#301026'});
+		}
+		if($(myParent).hasClass('kiss'))
+		{
+			$(this).css({'background-color':'#665454'});
+		}
 		
 		$(element).css({
 			'position':'absolute',
